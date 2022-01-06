34c65332a7ed444697ffb3f0fbef48089be75039
hymiside <vorochaev2004@icloud.com>
2022-01-06 20:50:56 +0500

    delete dependence

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue';
 import router from './router';
-import VueTextareaAutosize from "vue-textarea-autosize";
 
-createApp(App).use(router).use(VueTextareaAutosize).mount('#app');
+createApp(App).use(router).mount('#app');
